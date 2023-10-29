a134626cc6835eabd293b89d0edb833db0459ea0
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-10-29 10:05:37 +0100

    DarkMode is finished ! Now the video may display in light mode too

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  let video = document.querySelector('.video');
+  video.play();
+  video.muted = true;
+  video.loop = true;
+  video.controls = false;
+  video.preload = 'auto';
+});
 function scrollToDownload() {
     setTimeout(function () {
       var element = document.getElementById("download");
@@ -8,11 +16,21 @@ function scrollToDownload() {
 function toggleDarkMode(){
     let root = document.documentElement;
     let darkMode = document.querySelector('.dark-mode');
+    let src = document.querySelector('.source');
+    let video = document.querySelector('.video');
+    let overlay = document.querySelector('.overlay');
     if(root.style.getPropertyValue('--color-primary') == '#434b5e'){ 
       root.style.setProperty('--color-primary', '#e6ebf1'); //light
       root.style.setProperty('--color-secondary', '#d9dde9');
       root.style.setProperty('--font-color','black');
       root.style.setProperty('--highlight-color', 'rgba(0,0,0,0.5)');
+
+      video.pause();
+      src.src = "assets/onyx_light.mp4";
+      video.load();
+      video.play();
+
+      overlay.style.background = "rgba(255,255,255,0)";
       
       darkMode.innerHTML = "<img src='assets/light-mode.svg' />";
     }else{
@@ -20,6 +38,13 @@ function toggleDarkMode(){
       root.style.setProperty('--color-secondary', '#4b566a');
       root.style.setProperty('--font-color','white');
       root.style.setProperty('--highlight-color','transparent');
+
+      video.pause();
+      src.src = "assets/onyx_dark.mp4";
+      video.load();
+      video.play();
+
+      overlay.style.background = "rgba(0,0,0,0.5)";
 
       darkMode.innerHTML = "<img src='assets/dark-mode.svg' />";
     }
