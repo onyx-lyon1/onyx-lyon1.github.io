cf4d06dc1431c74d902f986cf09887b11c891f88
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2024-01-17 16:39:44 +0100

    fixed dblclick issue for languages + darkmode

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function scrollToDownload() {
     }, 100);
 }
 
+let darktoggled = false;
+
 const darkMode = document.querySelector('.dark-mode');
 
 darkMode.addEventListener('click', toggleDarkMode, false);
@@ -17,7 +19,7 @@ function toggleDarkMode(e){
     let srcwebm = document.querySelector('.sourcewebm');
     let video = document.querySelector('.video');
     let overlay = document.querySelector('.overlay');
-    if(root.style.getPropertyValue('--color-primary') == '#434b5e'){ 
+    if(!darktoggled){ 
       root.style.setProperty('--color-primary', '#e6ebf1'); //light
       root.style.setProperty('--color-secondary', '#d9dde9');
       root.style.setProperty('--font-color','black');
@@ -34,6 +36,8 @@ function toggleDarkMode(e){
       overlay.style.background = "rgba(255,255,255,0)";
       
       darkMode.innerHTML = "<img src='assets/light-mode.svg' />";
+
+      darktoggled = true;
     }else{
       root.style.setProperty('--color-primary', '#434b5e'); //dark
       root.style.setProperty('--color-secondary', '#4b566a');
@@ -51,6 +55,8 @@ function toggleDarkMode(e){
       overlay.style.background = "rgba(0,0,0,0.5)";
 
       darkMode.innerHTML = "<img src='assets/dark-mode.svg' />";
+
+      darktoggled = false;
     }
 }
 
@@ -98,14 +104,15 @@ swiper.params.autoplay.delay = 5000;
 //Language switcher
 
 document.querySelector('.lang').addEventListener('click', function() {
+    console.log("hey")
     if(this.getAttribute('language')==="FR"){
         this.setAttribute('language', 'EN');
-        document.querySelector('.lang-img').src = "assets/uk-flag.png";
+        document.querySelector('.lang-img').src = "assets/fr-flag.png";
         switchToEnglish();
     }
     else{
         this.setAttribute('language', 'FR');
-        document.querySelector('.lang-img').src = "assets/fr-flag.png";
+        document.querySelector('.lang-img').src = "assets/uk-flag.png";
         switchToFrench();
     }
 });
